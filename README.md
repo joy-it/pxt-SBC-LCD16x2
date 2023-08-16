21c3bd89b4ecae60eadeeae9950fada170a35a0e
Jens <jens.schwalenberg@gmail.com>
2023-08-16 09:40:14 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# pxt-SBC-LCD16x2
+# MakeCode Package for the Joy-IT SBC-LCD16x2
 This library provides a Microsoft Makecode package for the Joy-IT SBC-LCD16x2 16x2 I2C display module. See https://www.joy-it.net/products/SBC-LCD16x2 for more details.
+
+The 16x2 display can output texts on a total of 16 characters and over 2 lines. It is ideally suited for the quick output of status information or measurement results.
+
+**The 16x2 lcd display is connected via I2C on address 0x21.**
+
+### Initialization
+
+Because the display is an I2C-device, an initial initialization is required before use:
+
+```typescript
+SBCLCD16x2.lcd16x2Init()
+```
+
+### Power functions
+
+The display supports various power functions. These include power on, power off, backlight power on and backlight power off.
+
+```typescript
+// Turn LCD on
+SBCLCD16x2.lcd16x2TurnOn()
+// Turn LCD off
+SBCLCD16x2.lcd16x2TurnOff()
+// Turn backlight on
+SBCLCD16x2.lcd16x2BacklightOn()
+// Turn backlight off
+SBCLCD16x2.lcd16x2BacklightOff()
+```
+
+### Output text
+
+Text can be send to the display by using the **SBCLCD16x2.LCD16x2_showText(text, x, y)** function. The string to be displayed is passed with **text** and the position on the display is passed with **x** and **y**. The entire display output can be deleted with **SBCLCD16x2.LCD16x2_clear()**.
+
+```typescript
+// Show text at position 0 on line 0
+SBCLCD16x2.lcd16x2ShowText('Hello World', 0, 0)
+// Show text at position 2 on line 1
+SBCLCD16x2.lcd16x2ShowText('Hello World', 2, 1)
+// Clear output
+SBCLCD16x2.lcd16x2Clear()
+```
+
+### Shift text
+
+The whole output can be shifted to the left and right and can thus be moved over the display.
+
+```typescript
+// Shift text to the left
+SBCLCD16x2.lcd16x2ShiftLeft()
+// Shift text to the right
+SBCLCD16x2.lcd16x2ShiftRight()
+```
+
+### Cursor functions
+
+The display also offers the possibility to switch on the current cursor position. You can choose between a permanent and a blinking cursor. Of course, the cursor can also be switched off and moved back to the start position.
+
+```typescript
+// Show permanent cursor
+SBCLCD16x2.lcd16x2ShowCursor()
+// Show blinking cursor
+SBCLCD16x2.lcd16x2BlinkingCursor()
+// Move cursor to starting position
+SBCLCD16x2.lcd16x2ReturnHome()
+// Hide cursor
+SBCLCD16x2.lcd16x2HideCursor()
+```
---
+++ SBC-LCD16x2.ts
@@ -0,0 +1,233 @@
+/**
+  * SBC-LCD16x2 Block
+  */
+//% color="#275C6B" weight=100 icon="\uf26c" block="SBC-LCD16x2 Display"
+namespace SBCLCD16x2 {
+    const i2c_address = 0x21;
+    let Backlight: number;
+    let RegisterSelect: number;
+
+    function write(data: number) {
+        let enabled_value = data | 0x04;
+        let send = pins.createBuffer(4);
+
+        send.setNumber(NumberFormat.Int8LE, 0, 0x09);
+        send.setNumber(NumberFormat.Int8LE, 1, enabled_value);
+        send.setNumber(NumberFormat.Int8LE, 2, data);
+        pins.i2cWriteBuffer(i2c_address, send, false);
+    }
+
+    function set(data: number) {
+        data = (data & 0xF0) >> 1;
+        data = data | Backlight | RegisterSelect;
+        write(data);
+    }
+
+    function command(data: number) {
+        RegisterSelect = 0x00;
+        set(data);
+        set(data << 4);
+    }
+
+    function send_data(data: number) {
+        RegisterSelect = 0x02;
+        set(data);
+        set(data << 4);
+    }
+
+    /**
+     * Initializes the 16x2 LCD display
+     */
+    //% block="initialize 16x2LCD"
+    //% subcategory="LCD16x2"
+    //% weight=100
+    export function lcd16x2Init() {
+        Backlight = 0x80;
+        RegisterSelect = 0x00;
+
+        let buffer = pins.createBuffer(2);
+        buffer.setNumber(NumberFormat.Int8LE, 0, 0x00);
+        buffer.setNumber(NumberFormat.Int8LE, 1, 0xFF);
+        pins.i2cWriteBuffer(i2c_address, buffer, false);
+        buffer.setNumber(NumberFormat.Int8LE, 0, 0x06);
+        buffer.setNumber(NumberFormat.Int8LE, 1, 0x00);
+        pins.i2cWriteBuffer(i2c_address, buffer, false);
+        buffer.setNumber(NumberFormat.Int8LE, 0, 0x00);
+        buffer.setNumber(NumberFormat.Int8LE, 1, 0xFD);
+        pins.i2cWriteBuffer(i2c_address, buffer, false);
+        buffer.setNumber(NumberFormat.Int8LE, 0, 0x00);
+        buffer.setNumber(NumberFormat.Int8LE, 1, 0xF9);
+        pins.i2cWriteBuffer(i2c_address, buffer, false);
+        buffer.setNumber(NumberFormat.Int8LE, 0, 0x00);
+        buffer.setNumber(NumberFormat.Int8LE, 1, 0xF1);
+        pins.i2cWriteBuffer(i2c_address, buffer, false);
+        buffer.setNumber(NumberFormat.Int8LE, 0, 0x00);
+        buffer.setNumber(NumberFormat.Int8LE, 1, 0xE1);
+        pins.i2cWriteBuffer(i2c_address, buffer, false);
+        buffer.setNumber(NumberFormat.Int8LE, 0, 0x00);
+        buffer.setNumber(NumberFormat.Int8LE, 1, 0xC1);
+        pins.i2cWriteBuffer(i2c_address, buffer, false);
+        buffer.setNumber(NumberFormat.Int8LE, 0, 0x00);
+        buffer.setNumber(NumberFormat.Int8LE, 1, 0x81);
+        pins.i2cWriteBuffer(i2c_address, buffer, false);
+        buffer.setNumber(NumberFormat.Int8LE, 0, 0x00);
+        buffer.setNumber(NumberFormat.Int8LE, 1, 0x01);
+        pins.i2cWriteBuffer(i2c_address, buffer, false);
+        buffer.setNumber(NumberFormat.Int8LE, 0, 0x09);
+        buffer.setNumber(NumberFormat.Int8LE, 1, 0x00);
+        pins.i2cWriteBuffer(i2c_address, buffer, false);
+        basic.pause(1);
+
+        command(0x33);
+        command(0x32);
+        command(0x0C);
+        command(0x28);
+        command(0x06);
+        lcd16x2Clear();
+    }
+
+
+    /**
+     * Shows a text on the 16x2 display
+     * @param text The text that is supposed to be displayed
+     * @param x The x position on the display from 0 (most left position) to 15 (most right position)
+     * @param y The y position on the display from 0 (upper line) to 1 (lower line)
+     */
+    //% block="show String %s|at x %x|y %y on 16x2LCD"
+    //% subcategory="LCD16x2"
+    //% weight=90
+    //% x.min=0 x.max=15
+    //% y.min=0 y.max=1
+    export function lcd16x2ShowText(text: string, x: number, y: number): void {
+        let a: number;
+
+        if (y > 0) {
+            basic.pause(1)
+            a = 0xC0;
+        }
+        else {
+            basic.pause(1)
+            a = 0x80;
+        }
+        a += x;
+        command(a);
+
+        for (let i = 0; i < text.length; i++) {
+            send_data(text.charCodeAt(i));
+        }
+    }
+
+    /**
+     * Clears all outputs on the 16x2 LCD display
+     */
+    //% block="clear 16x2LCD"
+    //% subcategory="LCD16x2"
+    //% weight=85
+    export function lcd16x2Clear(): void {
+        command(0x01);
+    }
+
+    /**
+     * Shifts the current position to the left by 1 position
+     */
+    //% block="shift left on 16x2LCD"
+    //% weight=61
+    //% subcategory="LCD16x2"
+    export function lcd16x2ShiftLeft(): void {
+        command(0x18);
+    }
+
+    /**
+     * Shifts the current position to the right by 1 position
+     */
+    //% block="shift right on 16x2LCD"
+    //% weight=60
+    //% subcategory="LCD16x2"
+    export function lcd16x2ShiftRight(): void {
+        command(0x1C);
+    }
+
+    /**
+     * Shows the cursor on its current position
+     */
+    //% block="show cursor on 16x2LCD"
+    //% weight=50
+    //% subcategory="LCD16x2"
+    export function lcd16x2ShowCursor(): void {
+        command(0x0E);
+    }
+
+    /**
+     * Activates the blinking cursor
+     */
+    //% block="blinking Cursor on 16x2LCD"
+    //% weight=40
+    //% subcategory="LCD16x2"
+    export function lcd16x2BlinkingCursor(): void {
+        command(0x0F);
+    }
+
+    /**
+     * Hides the cursor on its current position
+     */
+    //% block="hide cursor on 16x2LCD"
+    //% weight=30
+    //% subcategory="LCD16x2"
+    export function lcd16x2HideCursor(): void {
+        command(0x0C);
+    }
+
+    /**
+     * Resets the cursor to its default position
+     */
+    //% block="set cursor back on 16x2LCD"
+    //% weight=20
+    //% subcategory="LCD16x2"
+    export function lcd16x2ReturnHome(): void {
+        command(0x02);
+    }
+
+    /**
+     * Turn the display off
+     */
+    //% block="turn off 16x2 LCD"
+    //% weight=19
+    //% subcategory="LCD16x2"
+    export function lcd16x2TurnOff(): void {
+        lcd16x2BacklightOff();
+        command(0x00);
+    }
+
+    /**
+ * Turn the display on
+ */
+    //% block="turn on 16x2 LCD"
+    //% weight=19
+    //% subcategory="LCD16x2"
+    export function lcd16x2TurnOn(): void {
+        lcd16x2BacklightOn();
+        command(0x04);
+    }
+
+    /**
+ * Turn the display off
+ */
+    //% block="turn off 16x2 LCD backlight"
+    //% weight=18
+    //% subcategory="LCD16x2"
+    export function lcd16x2BacklightOff(): void {
+        Backlight = 0x00;
+        set(0);
+    }
+
+    /**
+ * Turn the backlight on
+ */
+    //% block="turn on 16x2 LCD backlight"
+    //% weight=17
+    //% subcategory="LCD16x2"
+    export function lcd16x2BacklightOn(): void {
+        Backlight = 0x80
+        set(0);
+    }
+}
